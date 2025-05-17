2d6399cb0c29eabf240674e03eeb93509df73d53
Jangs <108041161+JangIkIk@users.noreply.github.com>
2025-05-17 16:47:47 +0900

    Feat: 공용버튼 생성 (#5)
    * Chore: Slot 설치
    
    * Style: app/styles 변수 위치변경
    
    * Feat: Button 생성
    
    * Remove: ButtonLinkble 삭제
    
    * Refactor: 공용 버튼 적용
    
    * Rename: Button.module.scss 이름으로 변경

--- widgets/header/presentational/HeaderPres.tsx
+++ widgets/header/presentational/HeaderPres.tsx
@@ -1,14 +1,17 @@
 // package
 import { type JSX } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 // slice
 import styles from '../styles/HeaderPres.module.scss';
 // layer
 import Logo from '@/shared/ui/logo';
 import PostsSearchCont from '@/features/search-input';
-import ButtonLinkable from '@/shared/ui/button-linkable';
+import Button from '@/shared/ui/button';
+
 
 export default function HeaderPres(): JSX.Element {
+
   return (
     <header className={styles.header}>
       {/* 로고 영역 */}
@@ -16,18 +19,38 @@ export default function HeaderPres(): JSX.Element {
 
       {/* 검색 폼 */}
       <PostsSearchCont/>
+
       {/* 네비게이션 */}
       <nav className={styles.nav}>
-        {/* <button className={styles.writeBtn}>새글작성</button> */}
-        <ButtonLinkable href="/member/story" text="새글작성" />
-        <ButtonLinkable text="로그인" />
-        <ButtonLinkable text="블로그페이지이동" href='/email/stories/1'/>
-        <button className={styles.iconBtn}>
+
+        <Button variants='purple' size='small' asChild>
+          <Link href='/member/story'>새글작성</Link>
+        </Button>
+
+        <Button variants='purple' size='small'>로그인</Button>
+
+        <button className={styles.iconBtn} >
           <Image src="/svgs/alarm.svg" alt="알림" width={24} height={24} />
         </button>
         <button className={styles.iconBtn}>
           <Image src="/svgs/profile.svg" alt="프로필" width={24} height={24} />
         </button>
+
+        {/* 개발편의성을 위한 임시버튼 */}
+        <Button variants='purple' size='small' asChild>
+          <Link href='/email/stories'>블로그페이지</Link>
+        </Button>
+
+        {/* 개발편의성을 위한 임시버튼 */}
+        <Button variants='purple' size='small' asChild>
+          <Link href='/email/stories/1'>상세페이지</Link>
+        </Button>
+
+        {/* 개발편의성을 위한 임시버튼 */}
+        <Button variants='purple' size='small' asChild>
+          <Link href='/member/setting'>설정페이지</Link>
+        </Button>
+
       </nav>
     </header>
   );
