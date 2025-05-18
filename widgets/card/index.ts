1a3b2d58f9c2ff135df13c85110e2f05c3860df5
Yooodh <eogus2604@hanmail.net>
2025-05-18 19:34:33 +0900

    Feat: CardListPres 컴포넌트 export 추가

--- widgets/card/index.ts
+++ widgets/card/index.ts
@@ -1,2 +1,3 @@
 export { default as SquareCardPres } from './presentational/SquareCardPres';
 export { default as LongCardPres } from './presentational/LongCardPres';
+export { default as CardListPres } from './presentational/CardListPres';
