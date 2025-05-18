d2741389abf53d27ae95c35d0b1a542310dffb38
Jangs <108041161+JangIkIk@users.noreply.github.com>
2025-05-18 16:44:58 +0900

    Feat: Login UI 생성 (#6)
    * Rename: settings.json 파일이동
    
    * Chore: 소셜Logo 파일 업로드
    
    * Docs: next.config.ts svgr 설정
    
    * Feat: login Modal 생성
    
    * Chore: svg build 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,46 @@
-import type { NextConfig } from "next";
-import path from 'path';
+import type { NextConfig } from 'next';
+import path from 'node:path';
 
 const nextConfig: NextConfig = {
-   sassOptions: {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  }, 
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
+  sassOptions: {
+    // SCSS/SASS import 시 사용할 경로를 지정
     includePaths: [path.join(__dirname, 'app/styles')],
   },
-  /* config options here */
 };
 
 export default nextConfig;
