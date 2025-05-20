bdffc093e8cee804e69899860404c46b8f2fd9c0
imi21123 <imi21123@naver.com>
2025-05-20 17:11:32 +0900

    refactor: 프로필 부분을 공통 프로필 컴포넌트로 교체

--- views/post-detail/presentational/CommentListPres.tsx
+++ views/post-detail/presentational/CommentListPres.tsx
@@ -1,7 +1,6 @@
-// package
-import Image from 'next/image';
 // slice
 import styles from '../styles/CommentListPres.module.scss';
+import Profile from '@/shared/ui/profile';
 
 type Comment = {
   userNickName: string;
@@ -18,19 +17,11 @@ export default function CommentListPres({ comments }: Props) {
     <div className={styles.commentList}>
       {comments.map((c, idx) => (
         <div className={styles.commentItem} key={idx}>
-          <div className={styles.commentUserBox}>
-            <Image
-              src="/svgs/profile.svg"
-              alt="user"
-              width={32}
-              height={32}
-              className={styles.commentProfile}
-            />
-            <div className={styles.commentContent}>
-              <div className={styles.commentUser}>{c.userNickName}</div>
-              <div className={styles.commentDate}>{c.date}</div>
-            </div>
-          </div>
+          <Profile
+            userNickName={c.userNickName}
+            date={c.date}
+            onClick={() => {}}
+          />
           <div className={styles.commentText}>{c.text}</div>
         </div>
       ))}
