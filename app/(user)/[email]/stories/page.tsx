f8c0a9d9826161e4ed2fcd7e0b69806652fc38ff
mjhn010 <mjhn010@naver.com>
2025-05-22 15:33:18 +0900

    Fix: myBlogPage 병합

--- app/(user)/[email]/stories/page.tsx
+++ app/(user)/[email]/stories/page.tsx
@@ -1,9 +1,34 @@
+//slice
 import ProfileCardCont from '@/views/profile-card/container/ProfileCardCont';
+import PostsSearchCont from '@/features/search-input';
 
+//style
+import styles from './styles.module.scss';
+import { LongCardPres } from '@/widgets/card';
+import CategoryListCont from '@/features/category-list/container/CategoryListCont';
 export default function Page() {
   return (
     <>
-      <ProfileCardCont />
+      <div className={styles.container}>
+        <div className={styles.cardFollowFlex}>
+          <ProfileCardCont />
+          <div className={styles.inputFlexBox}>
+            <div className={styles.inputContainer}>
+              <PostsSearchCont />
+            </div>
+          </div>
+          <div className={styles.categoryCardList}>
+            <CategoryListCont />
+            <div className={styles.cardList}>
+              <LongCardPres />
+              <LongCardPres />
+              <LongCardPres />
+              <LongCardPres />
+              <LongCardPres />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
