086daf624c0547031fcd18c611c6bd9ef91d0b7b
imi21123 <imi21123@naver.com>
2025-05-22 17:13:02 +0900

    Refactor: 알림 삭제 방식 변경

--- features/notification-delete/container/DeleteNotificationCont.tsx
+++ features/notification-delete/container/DeleteNotificationCont.tsx
@@ -1,13 +1,18 @@
 'use client';
 import Button from '@/shared/ui/button';
+import { useEffect } from 'react';
 
-interface Props {
-  onConfirm: () => void;
-}
+type Props = {
+  selectedIds: number[];
+};
 
-export default function DeleteNotificationCont({ onConfirm }: Props) {
+export default function DeleteNotificationCont({ selectedIds }: Props) {
+  const handleDelete = () => {
+    console.log('삭제하기', selectedIds);
+  };
+
   return (
-    <Button size="small" onClick={onConfirm}>
+    <Button size="small" onClick={handleDelete}>
       삭제하기
     </Button>
   );
