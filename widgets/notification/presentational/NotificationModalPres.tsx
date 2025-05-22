37103c25237e5a3fdf206b583d3b07f7f8b3b514
imi21123 <imi21123@naver.com>
2025-05-22 17:19:25 +0900

    Refactor: 알림 widget에 feature 반영

--- widgets/notification/presentational/NotificationModalPres.tsx
+++ widgets/notification/presentational/NotificationModalPres.tsx
@@ -3,32 +3,29 @@ import { Cross1Icon } from '@radix-ui/react-icons';
 // slice
 import styles from '../styles/NotificationModalPres.module.scss';
 import Button from '@/shared/ui/button';
-import { NotificationItemType } from '@/features/notification-list/types';
-import { NotificationItemCont } from '@/features/notification-list';
+import { NotificationListCont } from '@/features/notification-list';
+import { MarkAllReadCont } from '@/features/notification-mark-all-read';
+import { DeleteNotificationCont } from '@/features/notification-delete';
 
 type Props = {
-  notifications: NotificationItemType[];
   deleteMode: boolean;
   selectedIds: number[];
   onToggleSelect: (id: number) => void;
-  onDelete: () => void;
+  setAllNotificationIds: (ids: number[]) => void;
+  toggleSelectAll: () => void;
   onCancel: () => void;
   onToggleDeleteMode: () => void;
-  onSelectAll: () => void;
-  onMarkAllAsRead: () => void;
   onClose: () => void;
 };
 
 export default function NotificationModalPres({
-  notifications,
   deleteMode,
   selectedIds,
   onToggleSelect,
-  onDelete,
+  setAllNotificationIds,
+  toggleSelectAll,
   onCancel,
   onToggleDeleteMode,
-  onSelectAll,
-  onMarkAllAsRead,
   onClose,
 }: Props) {
   return (
@@ -37,54 +34,43 @@ export default function NotificationModalPres({
         <Cross1Icon />
       </button>
 
-      {notifications.length === 0 ? (
-        <p className={styles.empty}>알람 내역이 없어요</p>
-      ) : (
-        <>
-          <div className={styles.list}>
-            {notifications.map((n) => (
-              <NotificationItemCont
-                key={n.id}
-                item={n}
-                deleteMode={deleteMode}
-                selected={selectedIds.includes(n.id)}
-                onToggleSelect={onToggleSelect}
-              />
-            ))}
-          </div>
-          <div className={styles.footer}>
-            {deleteMode ? (
-              <div className={styles.deleteMode}>
-                <div className={styles.deleteModeInner}>
-                  <Button size="small" onClick={onDelete}>
-                    삭제하기 {selectedIds.length}
-                  </Button>
-                  <Button size="small" onClick={onSelectAll}>
-                    전체선택
-                  </Button>
-                </div>
-                <div className={styles.deleteModeInner}>
-                  <Button size="small" onClick={onCancel}>
-                    취소
-                  </Button>
-                  <Button size="small" onClick={onCancel}>
-                    완료
-                  </Button>
-                </div>
-              </div>
-            ) : (
-              <div className={styles.normalMode}>
-                <Button size="small" onClick={onMarkAllAsRead}>
-                  전체읽음
-                </Button>
-                <Button size="small" onClick={onToggleDeleteMode}>
-                  삭제하기
+      <>
+        <div className={styles.list}>
+          <NotificationListCont
+            deleteMode={deleteMode}
+            selectedIds={selectedIds}
+            setAllNotificationIds={setAllNotificationIds}
+            onToggleSelect={onToggleSelect}
+          />
+        </div>
+        <div className={styles.footer}>
+          {deleteMode ? (
+            <div className={styles.deleteMode}>
+              <div className={styles.deleteModeInner}>
+                <DeleteNotificationCont selectedIds={selectedIds} />
+                <Button size="small" onClick={toggleSelectAll}>
+                  전체선택
                 </Button>
               </div>
-            )}
-          </div>
-        </>
-      )}
+              <div className={styles.deleteModeInner}>
+                <Button size="small" onClick={onCancel}>
+                  취소
+                </Button>
+                <Button size="small" onClick={onCancel}>
+                  완료
+                </Button>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.normalMode}>
+              <MarkAllReadCont />
+              <Button size="small" onClick={onToggleDeleteMode}>
+                삭제하기
+              </Button>
+            </div>
+          )}
+        </div>
+      </>
     </div>
   );
 }
