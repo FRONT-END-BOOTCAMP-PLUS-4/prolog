06dc9e60100c02257723dd03de359ef9b12d61cc
jun Y <mjhn010@naver.com>
2025-05-22 16:03:52 +0900

    Feat: 팔로우 및 팔로워리스트 UI 생성 (#17)
    * "Feat: 팔로우 및 팔로워 UI 생성"
    
    * "Refactor: 팔로우 및 팔로워 코드 수정"
    
    * "Style: 공통 버튼 속성(secondary) 추가"
    
    * "Feat: 팔로우 및 팔로워 리스트 UI 생성 "
    
    * "Comment: import 주석 추가"
    
    * "Refactor: stories/page.tsx 더미 데이터삭제"
    
    * Feat: 카테고리UI 생성
    
    * Refactor: 프로필카드 이미지크기변경 및 팔로우UI추가
    
    * Style: 콘솔제거
    
    * Feat: 팔로우리스트 모달 UI 생성
    
    * Style: longCard css 스타일 수정
    
    * Fix: myBlogPage 병합
    
    * Remove: SubscriptionCout.tsx(네이밍오류) 삭제
    
    * Style: 콘솔제거
    
    * Style: page.tsx 스타일 수정

--- app/(user)/[email]/stories/page.tsx
+++ app/(user)/[email]/stories/page.tsx
@@ -1,33 +1,34 @@
+//slice
 import ProfileCardCont from '@/views/profile-card/container/ProfileCardCont';
+import PostsSearchCont from '@/features/search-input';
+
+//style
 import styles from './styles.module.scss';
-import Image from 'next/image';
-
-
+import { LongCardPres } from '@/widgets/card';
+import CategoryListCont from '@/features/category-list/container/CategoryListCont';
 export default function Page() {
-  // 더미 데이터
-  const user = {
-    nickname: '유저닉네임',
-    intro:
-      '안녕하세요! FE개발자입니다. 현재는 취준생이며 이러한 기술들을 사용중입니다. ...',
-    isFollowing: false,
-    follower: 123,
-    following: 45,
-  };
-  const posts = [
-    {
-      title: 'SSG에 대해서 알아보겠습니다',
-      desc: 'CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........',
-      tags: ['Tag', 'Tag'],
-      userNickName: 'userNickName',
-      date: '2025-01-01',
-      commentCount: 16,
-      loveCount: 16,
-    },
-    // ...더미 게시글
-  ];
-  const followerList = Array(6).fill({ userNickName: 'userNickName' });
-
-
-  return <></>;
-
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.cardFollowFlex}>
+          <ProfileCardCont />
+          <div className={styles.inputFlexBox}>
+            <div className={styles.inputContainer}>
+              <PostsSearchCont />
+            </div>
+          </div>
+          <div className={styles.categoryCardList}>
+            <CategoryListCont />
+            <div className={styles.cardList}>
+              <LongCardPres />
+              <LongCardPres />
+              <LongCardPres />
+              <LongCardPres />
+              <LongCardPres />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ features/subscription-list/presentational/SubscriptionListPres.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import { UserList } from '../types';
+import PostsSearchCont from '@/features/search-input';
+
+import styles from '../styles/subscriptionList.module.scss';
+type UserListProps = {
+  followerList: UserList[];
+  handleFollowListDisplay: () => void;
+  isFollow: boolean;
+};
+export default function SubscriptionListPres({
+  followerList,
+  handleFollowListDisplay,
+  isFollow,
+}: UserListProps) {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.flex}>
+          <div className={styles.flexColumn}>
+            <div className={styles.subscriptionBox}>
+              <div className={styles.followGap}>
+                <span
+                  className={
+                    isFollow ? styles.toggleTextActive : styles.toggleTextBasic
+                  }
+                >
+                  팔로워
+                </span>
+                <span className={styles.text}>/</span>
+                <span
+                  className={
+                    isFollow ? styles.toggleTextBasic : styles.toggleTextActive
+                  }
+                >
+                  팔로잉
+                </span>
+              </div>
+              {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+              <div
+                onClick={handleFollowListDisplay}
+                className={`${styles.toggle} ${isFollow ? '' : styles.movieToggle}`}
+              >
+                <span className={styles.toggleCurrent}></span>
+              </div>
+            </div>
+            <PostsSearchCont />
+          </div>
+        </div>
+        {followerList.map((item, index) => {
+          return (
+            <div className={styles.userList} key={index}>
+              <div>
+                <Image
+                  src={item.profileImg}
+                  alt="프로필이미지"
+                  width={32}
+                  height={32}
+                />
+              </div>
+              <div>{item.nickName}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
