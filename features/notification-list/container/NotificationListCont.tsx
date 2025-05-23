dbe6b91883c846095cd86dd3bb4b4da8942a5c72
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-05-23 10:45:56 +0900

    Refactor: 알림 로직 수정 (#29)
    * Feat: 알림 아이템 구현
    
    * Feat: 알림 삭제 기능 구현
    
    * Feat: 알림 전체읽음 기능 구현
    
    * Feat: 알림 모달 구현
    
    * Refactor: UI 관련 코드 presentation에서 처리하도록 수정
    
    * Refactor: 알림 아이템 id type string -> number로 변경
    
    * Refactor: 알림 아이템 -> 알림 리스트까지를 features로 분류
    
    * Refactor: 알림 삭제 방식 변경
    
    * Refactor: 알림 전체읽음 방식 변경
    
    * Refactor: 알림 widget에 feature 반영
    
    * Refactor: 중복 기능의 버튼 정리
    
    * remove: 분리한 기능 함수 삭제
    
    * refactor: 전체선택 구현 방식 변경
    
    * style: 스크롤바 숨김
    
    * remove: 안쓰는 import 제거

---
+++ features/notification-list/container/NotificationListCont.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { NotificationItemType } from '../types';
+import NotificationListPres from '../presentational/NotificationListPres';
+
+type Props = {
+  deleteMode: boolean;
+  selectedIds: number[];
+  setAllNotificationIds: (ids: number[]) => void;
+  onToggleSelect: (id: number) => void;
+};
+
+const dummyNotifications: NotificationItemType[] = [
+  {
+    id: 1,
+    userNickname: 'UserNickName1',
+    type: 'comment',
+    content: '댓글 내용',
+    postTitle: '게시글 제목',
+    postId: '1',
+    date: '2025-01-01',
+    isRead: false,
+  },
+  {
+    id: 2,
+    userProfileImage: '/svgs/profile.svg',
+    userNickname: 'UserNickName2',
+    type: 'post',
+    content: '게시글 내용',
+    postTitle: '게시글 제목',
+    postId: '2',
+    date: '2025-01-02',
+    isRead: false,
+  },
+  {
+    id: 3,
+    userNickname: 'UserNickName3',
+    type: 'comment',
+    content: '댓글 내용',
+    postTitle: '게시글 제목',
+    postId: '2',
+    date: '2025-01-03',
+    isRead: true,
+  },
+  {
+    id: 4,
+    userNickname: 'UserNickName4',
+    type: 'post',
+    content: '게시글 내용',
+    postTitle: '게시글 제목',
+    postId: '1',
+    date: '2025-01-04',
+    isRead: true,
+  },
+  {
+    id: 5,
+    userNickname: 'UserNickName5',
+    type: 'comment',
+    content: '댓글 내용',
+    postTitle: '게시글 제목',
+    postId: '1',
+    date: '2025-01-05',
+    isRead: false,
+  },
+  {
+    id: 6,
+    userNickname: 'UserNickName6',
+    type: 'post',
+    content: '게시글 내용',
+    postTitle: '게시글 제목',
+    postId: '2',
+    date: '2025-01-06',
+    isRead: false,
+  },
+];
+
+export default function NotificationListCont({
+  deleteMode,
+  selectedIds,
+  setAllNotificationIds,
+  onToggleSelect,
+}: Props) {
+  const [list, setList] = useState<NotificationItemType[]>([]);
+
+  useEffect(() => {
+    console.log('GET 알림 리스트');
+    setList(dummyNotifications);
+  }, []);
+
+  useEffect(() => {
+    setAllNotificationIds(list.map((item) => item.id));
+  }, [list, setAllNotificationIds]);
+
+  return (
+    <NotificationListPres
+      list={list}
+      deleteMode={deleteMode}
+      selectedIds={selectedIds}
+      onToggleSelect={onToggleSelect}
+    />
+  );
+}
---
+++ features/notification-list/index.ts
@@ -0,0 +1,1 @@
+export { default as NotificationListCont } from './container/NotificationListCont';
