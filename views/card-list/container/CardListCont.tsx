532d71faab5b5c72289bfb67ddc40bb3ee5922bc
Yooodh <eogus2604@hanmail.net>
2025-05-25 03:18:47 +0900

    Refactor: CardListCont 더미 데이터 구조 CardData 타입으로 통일 및 카드 데이터 개선

--- views/card-list/container/CardListCont.tsx
+++ views/card-list/container/CardListCont.tsx
@@ -5,19 +5,31 @@ import { useState } from 'react';
 
 // slice
 import CardListPres from '../presentational/CardListPres';
-
-type Item = {
-  id: number;
-  title?: string;
-  description?: string;
-};
+import { CardData, ViewType } from '../types';
 
 export default function CardListCont() {
-  const [viewType, setViewType] = useState<'card' | 'list'>('card');
-  const items: Item[] = Array.from({ length: 50 }, (_, i) => ({
-    id: i + 1,
-    title: `제목 ${i + 1}`,
-    description: `설명 ${i + 1}`,
+  const [viewType, setViewType] = useState<ViewType>('card');
+
+  const items: CardData[] = Array.from({ length: 30 }, (_, i) => ({
+    id: String(i + 1),
+    title: `CSR이란 ${i + 1}`,
+    desc: `CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ... ${i + 1}`,
+    tags: [
+      'Start',
+      'React',
+      'TypeScript',
+      'Next',
+      'HTML',
+      'CSS',
+      'Java',
+      'MySql',
+      'End',
+    ],
+    userNickName: `user ${i + 1}`,
+    date: '2025-01-01',
+    commentCount: i,
+    loveCount: Math.floor(Math.random() * 100),
+    imageUrl: '/svgs/image.svg',
   }));
 
   return (
