34822505758395a18465764c7360a32254bf877d
mjhn010 <mjhn010@naver.com>
2025-05-25 21:32:38 +0900

    Style: 카테고리 UI 반응형 수정 및 더미데이터 추가

--- features/category-list/container/CategoryListCont.tsx
+++ features/category-list/container/CategoryListCont.tsx
@@ -7,6 +7,16 @@ const categoryList = [
   { id: 2, categoryName: 'JAVA' },
   { id: 3, categoryName: 'JavaScript' },
   { id: 4, categoryName: '북마크' },
+  { id: 5, categoryName: '북마크' },
+  { id: 6, categoryName: '북마크' },
+  { id: 7, categoryName: '북마크' },
+  { id: 8, categoryName: '북마크' },
+  { id: 9, categoryName: '북마크' },
+  { id: 10, categoryName: '북마크' },
+  { id: 11, categoryName: '북마크' },
+  { id: 12, categoryName: '북마크' },
+  { id: 13, categoryName: '북마크' },
+  { id: 14, categoryName: '북마크' },
 ];
 export default function CategoryListCont() {
   const [category, setCategory] = useState<number>(1);
