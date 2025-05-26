15860ad5adcd43840412ce5d15a7d21d8f894d13
junik <wkdwnsdlr37@gmail.com>
2025-05-27 01:39:55 +0900

    Refactor: 임시 ssr 파일수정

--- app/login-ssr/page.tsx
+++ app/login-ssr/page.tsx
@@ -1,39 +1,79 @@
-import { auth } from '@/app/auth';
 import Image from 'next/image';
-import { signOut } from '@/app/auth';
+import { auth } from '@/app/(auth)/auth';
 
-async function logout() {
-    'use server';
-    await signOut();
+async function getData() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}/api/test`);
+    return await res.json();
+  } catch (error) {
+    return null;
+  }
 }
 
 export default async function Login() {
   const session = await auth();
+  const dataFromApi = await getData();
 
   if (session?.user) {
     return (
-      <form action={logout}>
-        <p>{session?.user.name}</p>
-        <p>{session?.user?.email}</p>
-        <p>{session?.user?.provider}</p>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          gap: '4px',
+        }}
+      >
+        <p
+          style={{
+            fontSize: '28px',
+            color: 'gray',
+            backgroundColor: '#f0f0f0',
+            padding: '4px 8px',
+            borderRadius: '4px',
+          }}
+        >
+          {session?.user?.provider}
+        </p>
+        <p style={{ fontSize: '20px', fontWeight: 'bold', color: '#333' }}>
+          {session?.user.name}
+        </p>
+        <p style={{ fontSize: '20px', color: 'gray', marginBottom: '16px' }}>
+          {session?.user?.email}
+        </p>
         {session?.user?.image && (
           <Image
-            src={session?.user?.image}
+            style={{ borderRadius: '50%' }}
+            src={session.user.image}
             width={48}
             height={48}
-            alt={session?.user?.name ?? 'image'}
+            alt={session.user.name ?? 'image'}
           />
         )}
-        {/* form형태로 사용하지 않고 onClick를 사용할시에는
-        CSR컴포넌트로 분리해야 합니다. */}
-        <button type='submit'>나가기</button>
-      </form>
+        <div style={{ display: 'flex', gap: '4px' }}>
+          <form action="/api/auth/signout" method="post">
+            <input type="hidden" name="/" value="/" />
+            <button
+              type="submit"
+              style={{
+                padding: '4px 8px',
+                borderRadius: '4px',
+                backgroundColor: '#f0f0f0',
+                color: '#333',
+                border: 'none',
+                cursor: 'pointer',
+              }}
+            >
+              로그아웃
+            </button>
+          </form>
+        </div>
+        {dataFromApi && <p>{dataFromApi}</p>}
+      </div>
     );
   }
 
-  return (
-    <div>
-      로그인 되어있지 않음
-    </div>
-  );
+  return <div>로그인 되어있지 않음</div>;
 }
