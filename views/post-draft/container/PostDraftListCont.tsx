9a82ad11b3db1507687cb25ff3bb66918e9234e4
uudeok <128568494+uudeok@users.noreply.github.com>
2025-05-26 15:54:10 +0900

    Chore: BlogPost 아키텍처 설계를 위한 기본 폴더 구조 추가 (#27)
    * Chore: 프로젝트 아키텍처 설계를 위한 기본 폴더 구조 추가
    
    * Feat: 게시글 생성 클린 아키텍처 초기 구성 및 누락된 BlogPost 컬럼 추가
    
    * Feat:  S3 이미지 업로드 및 게시글 생성 backend 로직 구현
    
    * Feat: 블로그 초안리스트 불러오는 백엔드 로직 구현
    
    * Refactor: Prisma 직접 호출 로직을 usecase 구조로 리팩토링
    
    * Feat: 임시저장 글 삭제하는 백엔드 로직 구현
    
    * Feat: useDebounce 훅 생성 및 임시저장 글 생성하는 백엔드 로직 작성
    
    * Feat: draftId 기반 임시 저장 처리 및 작성 알림 토스트 적용
    
    * Refactor: shared/lib 폴더 생성 및 Prisma 인스턴스 생성 후 관련 로직 수정
    
    * Fix: 클라이언트 전용 타입 분리 및 DTO와 역할 분리

--- views/post-draft/container/PostDraftListCont.tsx
+++ views/post-draft/container/PostDraftListCont.tsx
@@ -1,70 +1,36 @@
-import PostDraftListPres from '../presentational/PostDraftListPres';
+import { useEffect, useState } from 'react';
+
+import PostDraftPres from '../presentational/PostDraftListPres';
 import { PostDraftType } from '../types';
 
-const fakeData = [
-  {
-    createdAt: '2025-05-22',
-    title: '제목',
-    id: 10,
-  },
-  {
-    createdAt: '2025-05-21',
-    title: '무제',
-    id: 9,
-  },
-  {
-    createdAt: '2025-05-20',
-    title: '아무거나 작성',
-    id: 8,
-  },
-  {
-    createdAt: '2025-05-22',
-    title: '제목1',
-    id: 7,
-  },
-  {
-    createdAt: '2025-05-21',
-    title: '타입스크립트 공부하기',
-    id: 6,
-  },
-  {
-    createdAt: '2025-05-20',
-    title: '디자인패턴 공부',
-    id: 5,
-  },
-  {
-    createdAt: '2025-05-19',
-    title: '무한스크롤 구현하기',
-    id: 4,
-  },
-  {
-    createdAt: '2025-05-19',
-    title:
-      '제목이 엄청 길 경우 어떻게 될까 명령어를 필터링하거나 수정할 수 있는 함수입니다. 명령어를 필터링하거나 수정할 수 있는 함수입니다. 명령어를 필터링하거나 수정할 수 있는 함수입니다.',
-    id: 3,
-  },
-  {
-    createdAt: '2025-05-15',
-    title: '리액트에 대해서',
-    id: 2,
-  },
-  {
-    createdAt: '2025-04-02',
-    title: 'Next.js 란?',
-    id: 1,
-  },
-] as PostDraftType[];
-
 export default function PostDraftCont() {
-  const handleDeletePostDraft = (id: number) => {
+  const [draftList, setDraftList] = useState<PostDraftType[]>([]);
+
+  useEffect(() => {
+    /** 임시 저장 리스트 가져오는 로직 */
+    const getPostsDraftList = async () => {
+      const response = await fetch('/api/member/posts/drafts');
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch post draftList');
+      }
+
+      const result = await response.json();
+      setDraftList(result.data);
+    };
+
+    getPostsDraftList();
+  }, []);
+
+  /** 임시 저장 글 삭제 로직 */
+  const handleDeletePostDraft = async (draftId: number) => {
     const confirmed = window.confirm('삭제하시겠습니까?');
+
     if (confirmed) {
-      console.log(`${id}번 임시글 삭제`);
-      // 실제 삭제 로직 작성 예정
+      console.log(`${draftId}번 임시글 삭제`);
+      await fetch(`/api/member/posts/drafts/?draftId=${draftId}`);
     }
   };
 
-  return (
-    <PostDraftListPres drafts={fakeData} onDelete={handleDeletePostDraft} />
-  );
+  return <PostDraftPres drafts={draftList} onDelete={handleDeletePostDraft} />;
 }
