8895ce0d2e58016168dd5dfa9d9cb68bbb07851a
mjhn010 <mjhn010@naver.com>
2025-05-28 01:26:15 +0900

    Refactor: myBlogCardList 더미데이터 및selectBox 추가

--- views/story/myblog-card-list/presentational/MyBlogCardListPres.tsx
+++ views/story/myblog-card-list/presentational/MyBlogCardListPres.tsx
@@ -1,14 +1,42 @@
+import { SelectCont } from '@/features/select';
 import { LongCardPres } from '@/widgets/card';
-import { CardData } from '@/widgets/card/types';
+import { MyBlogCardData } from '../types';
+import styles from '../styles/myBlogCardList.module.scss';
+import PostsSearchCont from '@/features/search-input/container/PostsSearchCont';
 type dataProps = {
-  data: CardData[];
+  data: MyBlogCardData[];
+  sort: 'latest' | 'popular' | 'bookMark';
+  setSort: (sort: 'latest' | 'popular' | 'bookMark') => void;
+  items: MyBlogCardData[];
+  sortOptions: { label: string; value: string }[];
 };
-export default function MyBlogCardListPres({ data }: dataProps) {
+export default function MyBlogCardListPres({
+  sort,
+  setSort,
+  items,
+  sortOptions,
+  data,
+}: dataProps) {
   return (
     <>
-      {data.map((item) => (
-        <LongCardPres key={item.id} data={item} />
-      ))}
+      <div className={styles.myBlogContainer}>
+        <div className={styles.selectContainer}>
+          <PostsSearchCont />
+          <SelectCont
+            options={sortOptions}
+            value={sort}
+            onChange={(val) =>
+              setSort(val as 'latest' | 'popular' | 'bookMark')
+            }
+            className={styles.selectWrap}
+          />
+        </div>
+        {data.map((item) => (
+          <div className={styles.cardContainer} key={item.id}>
+            <LongCardPres data={item} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
