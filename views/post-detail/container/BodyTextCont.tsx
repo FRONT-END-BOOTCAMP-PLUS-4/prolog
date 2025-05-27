42799c563dbea080ca9cf4f40860eb7f2dbc8a26
imi21123 <imi21123@naver.com>
2025-05-27 14:38:25 +0900

    Feat: 본문에 태그 리스트 영역 추가

--- views/post-detail/container/BodyTextCont.tsx
+++ views/post-detail/container/BodyTextCont.tsx
@@ -1,8 +1,39 @@
 import BodyTextPres from '../presentational/BodyTextPres';
 
-export default function BodyTextCont() {
-  const dummyBody = `
-CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........  `;
+const dummyPost = {
+  id: 1,
+  content: `
+CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........CSR은 클라이언트 사이드 렌더링이라고 합니다. 이는 SEO에 좋지 못하지만, 좀더 인터렉티브한 디자인에는 좋은경험을 ........  `,
+  tags: [
+    'tech',
+    'code',
+    'react',
+    'javascript',
+    'web development',
+    'frontend',
+    'programming',
+    'development',
+    'ui/ux',
+    'design',
+    'tutorial',
+    'guide',
+    'tips',
+    'best practices',
+    'performance',
+    'accessibility',
+    'responsive design',
+    'testing',
+    'optimization',
+    'deployment',
+  ],
+};
 
-  return <BodyTextPres body={dummyBody} />;
+export default function BodyTextCont() {
+  const { content, tags } = dummyPost;
+
+  return (
+    <>
+      <BodyTextPres body={content} tags={tags} />
+    </>
+  );
 }
--- views/post-detail/presentational/BodyTextPres.tsx
+++ views/post-detail/presentational/BodyTextPres.tsx
@@ -1,10 +1,21 @@
 // slice
+import Tag from '@/shared/ui/tag';
 import styles from '../styles/BodyTextPres.module.scss';
 
 type Props = {
   body: string;
+  tags: string[];
 };
 
-export default function BodyTextPres({ body }: Props) {
-  return <div className={styles.bodyText}>{body}</div>;
+export default function BodyTextPres({ body, tags }: Props) {
+  return (
+    <>
+      <div className={styles.bodyText}>{body}</div>
+      <div className={styles.tagList}>
+        {tags.map((tag, idx) => (
+          <Tag key={`${tag}-${idx}`}>{tag}</Tag>
+        ))}
+      </div>
+    </>
+  );
 }
