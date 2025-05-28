b6297d7cb2fd10cf366924dd975b60c552f00702
jun Y <mjhn010@naver.com>
2025-05-28 17:36:56 +0900

    Refactor: myBlogCardList 더미데이터 및selectBox 추가 (#53)
    * Refactor: myBlogCardList 더미데이터 및selectBox 추가
    
    * Fix: email/stories page.tsx 수정

---
+++ views/story/myblog-card-list/types.ts
@@ -0,0 +1,12 @@
+export type MyBlogCardData = {
+  id: string;
+  title: string;
+  desc: string;
+  tags: string[];
+  userNickName: string;
+  date: string;
+  commentCount: number;
+  loveCount: number;
+  imageUrl: string;
+  isBookMarked: boolean;
+};
