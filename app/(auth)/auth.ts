832eaf9fdfe0991adaeb21d50db33f5c0a7a106c
junik <wkdwnsdlr37@gmail.com>
2025-05-29 06:11:08 +0900

    Refactor: auth 재설정

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,29 +1,13 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 import Github from 'next-auth/providers/github';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-
-// const prisma = new PrismaClient();
+import prisma from '@/shared/lib/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // adapter: {
-  //   ...PrismaAdapter(prisma),
-  //   createUser: async (data) => {
-  //     return await prisma.user.create({
-  //       data: {
-  //         nickname: data.name,
-  //         email: data.email,
-  //         profileImg: data.image
-  //       }
-  //     })
-  //   }
-  // },
   session: {
     strategy: 'jwt',
     maxAge: 60 * 60,
   },
-  cookies: {},
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
@@ -31,15 +15,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       authorization: {
         params: {
           prompt: 'consent',
-          // prompt: "none"     // 동의 화면 없이 자동 로그인 (이미 승인된 경우)
-          // prompt: "consent"  // 항상 동의 화면 표시
-          // prompt: "login"    // 항상 로그인 화면 표시
           access_type: 'offline',
-          // offline: 사용자가 오프라인일 때도 앱이 API에 접근할 수 있도록 refresh token 제공
-          // online (기본값): Access token만 제공, refresh token 없음
           response_type: 'code',
-          // code: 보안이 강화된 Authorization Code Flow 사용
-          // token: 덜 안전한 Implicit Flow (SPA에서 주로 사용)
         },
       },
     }),
@@ -49,16 +26,46 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
+    async signIn({ user, account }) {
+
+        // Oauth에서 제공자가 없다면 로그인 불가
+        if(!account) return false;
+
+        // 새로운 사용자일경우 db에 값저장
+        if (user.email && user.name) {
+          const existingUser = await prisma.user.findUnique({
+            where: { email: user.email, provider: account.provider },
+          });
+
+          if (!existingUser) {
+            await prisma.user.create({
+              data: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                profileImg: user.image,
+                provider: account.provider,
+              },
+            })
+          };
+          user.id = existingUser ? existingUser.id : user.id
+        }
+
+      return true;
+    },
+
+    async jwt({ token, user, account}) {
       if (account) {
         token.accessToken = account.access_token;
         token.refreshToken = account.refresh_token;
         token.provider = account.provider;
+        token.userId = user.id;
       }
       return token;
     },
     async session({ session, token }) {
-      session.user.provider = token.provider as string;
+      session.user.provider = token.provider;
+      session.user.id = token.userId as string;
       return session;
     },
   },
