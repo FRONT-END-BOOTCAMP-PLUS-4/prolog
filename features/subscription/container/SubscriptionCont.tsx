a67a79d2bb5bb4a9a38f7b4c897b448f60953e4a
mjhn010 <mjhn010@naver.com>
2025-05-30 00:52:57 +0900

    Feat: subscription 백엔드 기능 구현

--- features/subscription/container/SubscriptionCont.tsx
+++ features/subscription/container/SubscriptionCont.tsx
@@ -1,15 +1,26 @@
 'use client';
 //package
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 //slice
 import SubscriptionPres from '../presentational/SubscriptionPres';
+import { useSession } from 'next-auth/react';
 
 export default function SubscriptionCont() {
+  const { data: session, status } = useSession();
+  console.log(session);
   const [isFollowing, setIsFollowing] = useState(false);
   const followerHandler = () => {
     setIsFollowing((prev) => !prev);
   };
-
+  useEffect(() => {
+    const subscribeHandler = async () => {
+      if (status === 'authenticated') {
+        const { id } = session.user;
+        const response = await fetch(`/api/subscription?user=${id}`);
+      }
+      subscribeHandler();
+    };
+  }, []);
   return (
     <SubscriptionPres
       isFollowing={isFollowing}
