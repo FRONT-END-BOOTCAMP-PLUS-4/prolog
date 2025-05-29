abed0f9f507371060180d19ec32f1e886d4ecb9d
Yooodh <eogus2604@hanmail.net>
2025-05-29 14:34:38 +0900

    Feat: 게시글 전체 조회 도메인 인터페이스 및 필터 타입 정의

---
+++ back/posts/domain/PostListAllRepository.ts
@@ -0,0 +1,18 @@
+import { BlogPost } from '@/app/generated/prisma';
+
+export interface GetPostListAllFilter {
+  name?: string;
+  tags?: string[];
+  title?: string;
+  content?: string;
+}
+
+export interface PostListAllRepository {
+  findAll(filters: GetPostListAllFilter): Promise<
+    (BlogPost & {
+      likes: { id: number }[];
+      notification: { id: number }[];
+      user: { id: string; name: string };
+    })[]
+  >;
+}
