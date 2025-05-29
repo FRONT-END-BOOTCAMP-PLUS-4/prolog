1ce07ca6391ff700d43075931f1cf5ed030b5030
uudeok <128568494+uudeok@users.noreply.github.com>
2025-05-29 17:44:47 +0900

    Refactor : 이미지 업로드 로직 클린 아키텍처로 분리 및 API 경로 변경 (#58)
    * Refactor: 바텀시트 닫는 함수를 Prop 주입에서 store 직접 호출하도록 변경
    
    * Refactor : 기존 S3 이미지 업로드 로직을 클린 아키텍처 구조로 분리
    
    * Fix : 불필요한 함수 삭제

--- back/upload/infra/UploadImgToS3Repository.ts
+++ back/upload/infra/UploadImgToS3Repository.ts
@@ -1,5 +1,6 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { NextRequest } from 'next/server';
+
+import { UploadImgRepository } from '../domain/UploadImgRepository';
 
 const Bucket = process.env.AWS_BUCKET_NAME;
 
@@ -11,15 +12,12 @@ const s3 = new S3Client({
   },
 });
 
-export async function POST(req: NextRequest) {
-  try {
-    const formData = await req.formData();
-    const files = formData.getAll('img') as File[];
-
+export class PrUploadImgS3Repository implements UploadImgRepository {
+  async uploadImg(files: File[]): Promise<string[]> {
     const uploadPromises = files.map(async (file) => {
       const Body = Buffer.from(await file.arrayBuffer());
       const extension = file.name.split('.').pop();
-      const randomStr = Math.random().toString(36).substring(2, 8); // 랜덤 문자열
+      const randomStr = Math.random().toString(36).substring(2, 8);
       const timestamp = Date.now();
       const fileName = `${timestamp}_${randomStr}.${extension}`;
       const ContentType = file.type || 'image/jpeg';
@@ -37,20 +35,23 @@ export async function POST(req: NextRequest) {
       return imageUrl;
     });
 
-    const imgUrls = await Promise.all(uploadPromises);
+    const results = await Promise.allSettled(uploadPromises);
 
-    return new Response(
-      JSON.stringify({
-        data: imgUrls.length === 1 ? imgUrls[0] : imgUrls,
-        message: 'OK',
-      }),
-      { status: 200 },
-    );
-  } catch (error) {
-    console.error('Error uploading files:', error);
-    return new Response(
-      JSON.stringify({ message: '파일 업로드 중 오류가 발생했습니다.' }),
-      { status: 500 },
-    );
+    // 성공한 것만 추출
+    const successfulUrls = results
+      .filter(
+        (result): result is PromiseFulfilledResult<string> =>
+          result.status === 'fulfilled',
+      )
+      .map((result) => result.value);
+
+    // 실패한 로그 출력 (선택)
+    results
+      .filter((result) => result.status === 'rejected')
+      .forEach((result, idx) => {
+        console.error(`업로드 실패 (index ${idx}):`, result.reason);
+      });
+
+    return successfulUrls;
   }
 }
