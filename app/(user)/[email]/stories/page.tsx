129c165a07bcab3591b7781d9eef79bff1ca897f
jun Y <mjhn010@naver.com>
2025-05-30 00:27:21 +0900

    Feat: 유저프로필 백엔드 로직 구현 (#60)
    * Refactor: 비로그인 또는 token이 없을경우 처리
    
    * Feat: 유저프로필정보 백엔드 로직 구현
    
    * Refactor: 더미데이터 삭제 데이터바인딩

--- app/(user)/[email]/stories/page.tsx
+++ app/(user)/[email]/stories/page.tsx
@@ -5,7 +5,18 @@ import ProfileCardCont from '@/views/story/profile-card/container/ProfileCardCon
 import styles from './styles.module.scss';
 import CategoryListCont from '@/features/category-list/container/CategoryListCont';
 import MyBlogCardListCont from '@/views/story/myblog-card-list/container/MyBlogCardListCont';
-export default function Page() {
+import { auth } from '@/app/(auth)/auth';
+
+export default async function Page() {
+  const session = await auth();
+  //나중에 UI 처리 또는 처리 방식 고민해보기 !
+  if (!session) {
+    return (
+      <>
+        <div>로그인 후 이용하실 수 있는 서비스입니다.</div>
+      </>
+    );
+  }
   return (
     <>
       <div className={styles.container}>
