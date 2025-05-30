71b3f6fe6c7eeb3954ca62724f009988532048aa
mjhn010 <mjhn010@naver.com>
2025-05-30 17:25:27 +0900

    Chore: 커밋 오류 파일 커밋 정정

--- app/api/member/subscription/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-// 쿼리문에서 제어 가능할수도? 화이팅입니다 !
-export async function GET(request: NextRequest) {
-  const useId = request.nextUrl.searchParams.get('id');
-  console.log(useId);
-}
-export async function POST(request: NextRequest) {}
