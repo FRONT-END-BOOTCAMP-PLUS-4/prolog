f3fb53f84e9b716b0cdb887849d9b47d0ff2d5ee
mjhn010 <mjhn010@naver.com>
2025-05-30 16:50:49 +0900

    Refactor: 회원 및 비회원 접근 가능 크드변경 및 endPoint경로 변경

--- app/api/member/route.ts
+++ app/api/member/route.ts
@@ -1,32 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-import { PrProfileRepository } from '@/back/story/profile/infra/PrProfileRepository';
-import { GetProfileUseCase } from '@/back/story/profile/application/usecase/GetProfileUsecase';
-
-export async function GET(request: NextRequest) {
-  try {
-    const clientEmail = request.nextUrl.searchParams.get('email');
-    const userData = await getToken({
-      req: request,
-      secret: process.env.AUTH_SECRET,
-    });
-
-    if (!userData || clientEmail !== userData.email) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    }
-    const { userId } = userData as { userId: string; email: string };
-    const repository = new PrProfileRepository();
-    const usecase = new GetProfileUseCase(repository);
-    const userProfile = await usecase.execute(userId);
-    return NextResponse.json(userProfile);
-  } catch (error) {
-    return NextResponse.json(
-      { message: 'Internal Server Error' },
-      { status: 500 },
-    );
-  }
-}
-
+export async function GET() {}
 export async function POST() {}
 export async function PUT() {}
 export async function DELETE() {}
