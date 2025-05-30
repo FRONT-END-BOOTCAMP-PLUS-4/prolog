647d1aa432e982cf5803f9e8264991971ca8390c
imi21123 <imi21123@naver.com>
2025-05-30 10:38:24 +0900

    Refactor: 공용 프로필 컴포넌트에서 유저 페이지로 이동하는 로직 추가

--- app/(user)/[email]/stories/[id]/page.tsx
+++ app/(user)/[email]/stories/[id]/page.tsx
@@ -42,11 +42,11 @@ export default async function Page({ params }: { params: { id: string } }) {
       <div className={styles.profileLayout}>
         {/* 프로필/팔로우 바 */}
         <div className={styles.profileBar}>
-          {/* <Profile
-            userNickName={post.userNickName}
+          <Profile
+            userNickName={post.nickname}
             date={post.createdAt}
             userEmail={post.userEmail}
-          /> */}
+          />
           <Button
             style={{ padding: '0.2rem 0.5rem', fontSize: '13px' }}
             variants="active"
