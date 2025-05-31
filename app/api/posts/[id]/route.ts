6e103ef71bb99b4707b67ffb6b3cdd72c4bc133a
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-05-31 23:51:46 +0900

    Feat: 게시글 상세조회 백엔드 작업 (#63)
    * Feat: csr 컴포넌트로 변경
    
    * Refactor: 댓글 및 게시글 수정 방식 변경
    
    * Refactor: 댓글 및 게시글 삭제 방식 변경
    
    * Feat: 게시글 상세조회 백엔드 로직
    
    * Feat: 게시글 상세조회 api 연결
    
    * Feat: 북마크 데이터 연결
    
    * Feat: 좋아요 데이터 연결
    
    * Feat: AI 요약 데이터 연결
    
    * Feat: 게시글 본문&태그 리스트 데이터 연결
    
    * Feat: dayjs 적용
    
    * Feat: 데이터 형식에 userEmail 추가
    
    * Refactor: 공용 프로필 컴포넌트에서 유저 페이지로 이동하는 로직 추가

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,3 +1,21 @@
+import { GetPostUsecase } from '@/back/posts/application/usecases/GetPostUsecase';
+import { PrPostRepository } from '@/back/posts/infra/PrPostsRepository';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {}
+export async function GET() {
+  try {
+    const postId = 1; // 예시로 고정된 ID 사용, 실제로는 URL 파라미터에서 가져와야 함
+
+    const repository = new PrPostRepository();
+    const getPostUsecase = new GetPostUsecase(repository);
+    const post = await getPostUsecase.execute(postId);
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    console.error('게시글 불러오기 오류:', error);
+    return NextResponse.json(
+      { message: '서버 오류가 발생했습니다.' },
+      { status: 500 },
+    );
+  }
+}
