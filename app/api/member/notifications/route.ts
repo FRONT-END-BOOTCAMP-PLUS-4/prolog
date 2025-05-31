a5809142d0d6915f86c4cd4de167b7e3365ec82f
Jangs <108041161+JangIkIk@users.noreply.github.com>
2025-06-01 01:05:52 +0900

    Feat: alarm delete (#73)
    * Refactor: seed 내용변경
    
    * Feat: 임시 fetch
    
    * Feat: domain 생성
    
    * Feat: application 생성
    
    * Feat: infra 생성
    
    * Feat: api 생성
    
    * Feat: domain 생성
    
    * Feat: application 생성
    
    * Feat: infra 생성
    
    * Feat: api 생성
    
    * Refactor: 코드정렬

--- app/api/member/notifications/route.ts
+++ app/api/member/notifications/route.ts
@@ -4,6 +4,7 @@ import { getToken } from 'next-auth/jwt';
 import { PrAlarmRepository } from '@/back/alarm/infra/PrAlarmRepository';
 import { GetAlarmListUsecase } from '@/back/alarm/application/usecases/GetAlarmListUsecase';
 import { PutAlarmCheckUsecase } from '@/back/alarm/application/usecases/PutAlarmCheckUsecase';
+import { DeleteAlarmUsecase } from '@/back/alarm/application/usecases/DeleteAlarmUseCase';
 
 export async function GET(req: NextRequest) {
   const token = await getToken({
@@ -36,10 +37,39 @@ export async function PUT(req: NextRequest) {
   }
 
   try {
-    const body = (await req.json() as {alarmId: number});
+    const body = (await req.json()) as { alarmId: number };
     const prAlarmRepository = new PrAlarmRepository();
     const putAlarmCheckUsecase = new PutAlarmCheckUsecase(prAlarmRepository);
-    const execute= await putAlarmCheckUsecase.execute(token.userId, body.alarmId);
+    const execute = await putAlarmCheckUsecase.execute(
+      token.userId,
+      body.alarmId,
+    );
+    return NextResponse.json({ status: 200, data: execute });
+  } catch (error) {
+    console.log(error);
+    // useCases Error 대기
+    // infra Error 대기
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+  });
+  if (!token) {
+    return NextResponse.json({ status: 401, message: 'Unauthorized' });
+  }
+
+
+  try {
+    const body = (await req.json()) as { alarmId: number[] };
+    const prAlarmRepository = new PrAlarmRepository();
+    const deleteAlarmUsecase = new DeleteAlarmUsecase(prAlarmRepository);
+    const execute = await deleteAlarmUsecase.execute(
+      body.alarmId,
+      token.userId,
+    );
     return NextResponse.json({ status: 200, data: execute });
   } catch (error) {
     console.log(error);
