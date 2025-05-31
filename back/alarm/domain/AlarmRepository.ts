e5718fe811678b95ce0281986250571b64ebede7
junik <wkdwnsdlr37@gmail.com>
2025-06-01 00:51:19 +0900

    Feat: domain 생성

--- back/alarm/domain/AlarmRepository.ts
+++ back/alarm/domain/AlarmRepository.ts
@@ -1,5 +1,14 @@
 export interface AlarmRepository {
-  findAll(receiverId: string): Promise<Array<{id: number, type: number, postsId:number, receiverId: string, senderId:string}>>;
-  check(receiverId: string, alarmId: number): Promise<boolean>
+  findAll(receiverId: string): Promise<
+    Array<{
+      id: number;
+      type: number;
+      postsId: number;
+      receiverId: string;
+      senderId: string;
+    }>
+  >;
+  check(receiverId: string, alarmId: number): Promise<boolean>;
   checkAll(receiverId: string): Promise<boolean>;
+  deleteId(alarmId: number[], receiverId: string): Promise<boolean>;
 }
