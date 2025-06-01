02d0eeae8209e94d948a611d1ab084fa2519fa95
mjhn010 <mjhn010@naver.com>
2025-06-01 22:01:20 +0900

    Feat: subscribe 신청 취소 백엔드 로직 구현

--- features/subscription/container/SubscriptionCont.tsx
+++ features/subscription/container/SubscriptionCont.tsx
@@ -1,26 +1,47 @@
 'use client';
 //package
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 //slice
 import SubscriptionPres from '../presentational/SubscriptionPres';
 import { useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 export default function SubscriptionCont() {
   const { data: session, status } = useSession();
-  console.log(session);
   const [isFollowing, setIsFollowing] = useState(false);
+  const isFirstRender = useRef(true);
+  const pathname = usePathname();
+  const segments = pathname.split('/');
+  const email = segments[1]; // "mjhn010@naver.com"
+  const prefix = email?.split('@')[0]; // "mjhn010"
+
   const followerHandler = () => {
     setIsFollowing((prev) => !prev);
   };
   useEffect(() => {
-    const subscribeHandler = async () => {
-      if (status === 'authenticated') {
-        const { id } = session.user;
-        const response = await fetch(`/api/subscription?user=${id}`);
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    const toggleSubscribeHandler = async () => {
+      if (status === 'unauthenticated') {
+        throw new Error('로그인이 필요합니다');
       }
-      subscribeHandler();
+      if (status === 'authenticated') {
+        console.log(session.user);
+        const { email, id } = session.user;
+        try {
+          const response = await fetch('/api/member/subscription', {
+            method: 'POST',
+            body: JSON.stringify({ isFollowing, prefix, id }),
+          });
+          const data = await response.json();
+          console.log(data);
+        } catch (error) {}
+      }
     };
-  }, []);
+    toggleSubscribeHandler();
+  }, [isFollowing]);
   return (
     <SubscriptionPres
       isFollowing={isFollowing}
