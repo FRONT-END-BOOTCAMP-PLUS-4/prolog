51afedf825a58346980d6515be52732c903bf2c9
junik <wkdwnsdlr37@gmail.com>
2025-06-01 19:05:58 +0900

    Refactor: findAll returnType변경

--- back/alarm/domain/AlarmRepository.ts
+++ back/alarm/domain/AlarmRepository.ts
@@ -1,13 +1,19 @@
+export type findAllReturn = {
+  id: number;
+  type: number;
+  postsId: number;
+  senderId: string;
+  checkStatus: number;
+  createAt: Date;
+  title: string;
+  content: string;
+  senderName: string;
+  senderProfileImg: string | null;
+  comment_content: string | null;
+};
+
 export interface AlarmRepository {
-  findAll(receiverId: string): Promise<
-    Array<{
-      id: number;
-      type: number;
-      postsId: number;
-      receiverId: string;
-      senderId: string;
-    }>
-  >;
+  findAll(receiverId: string): Promise<findAllReturn[]>;
   check(receiverId: string, alarmId: number): Promise<boolean>;
   checkAll(receiverId: string): Promise<boolean>;
   deleteId(alarmId: number[], receiverId: string): Promise<boolean>;
