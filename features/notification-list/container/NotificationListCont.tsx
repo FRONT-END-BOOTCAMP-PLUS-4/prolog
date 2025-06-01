3b846b43cd1eca058f97b52c8599b29b0ca8c201
junik <wkdwnsdlr37@gmail.com>
2025-06-02 03:00:38 +0900

    Refactor: Notification-list 구조분리

--- features/notification-list/container/NotificationListCont.tsx
+++ features/notification-list/container/NotificationListCont.tsx
@@ -1,18 +1,9 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import NotificationListPres from '../presentational/NotificationListPres';
 import { type Notification} from '../types'
 
 export default function NotificationListCont() {
-  // 상위 props 대기
-  const [isSelect, setIsSelect] = useState<boolean>(false);
-  // 상위 props 대기
-  const [selectList, setSelectList] = useState<number[]>([]);
-  const clickSelecterId = (idx: number) => {
-    setSelectList([...selectList, idx]);
-  };
-
   const [notificationList, setNotificationList] = useState<Notification[]>([]);
   
   useEffect(() => {
@@ -21,11 +12,5 @@ export default function NotificationListCont() {
       .then((data) => setNotificationList(data.data));
   }, []);
 
-  return (
-    <NotificationListPres
-      notificationList={notificationList}
-      isSelect={isSelect}
-      selecterId={clickSelecterId}
-    />
-  );
+  return { notificationList };
 }
--- features/notification-list/presentational/NotificationListPres.tsx
+++ features/notification-list/presentational/NotificationListPres.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 // package
+import { useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { CheckCircledIcon } from '@radix-ui/react-icons';
@@ -11,82 +12,90 @@ import { type Notification } from '../types';
 
 type Props = {
   notificationList: Notification[];
-  isSelect: boolean;
+  isCheck: boolean;
+  selectList: number[];
   selecterId: (notificationId: number) => void;
 };
 
 export default function NotificationListPres(props: Props) {
-  const { notificationList, selecterId, isSelect } = props;
 
+  const { notificationList, selecterId, selectList, isCheck } = props;
+
+  // check
+  const isSelectd = (idx: number) => selectList.includes(idx);  
+
+  // router
   const router = useRouter();
   const blogPageRouter = (userName: string, postId: number) => {
     router.push(`/${userName}/stories/${postId}`);
   };
 
+  // empty
   if (!notificationList.length) {
     return <p className={styles.empty}>알람 내역이 없어요</p>;
   }
 
   return (
     <div className={styles.container}>
-      {notificationList.map((notification) => {
-        return (
-          <div
-            key={notification.id}
-            className={styles.notification}
-            onClick={() =>
-              blogPageRouter(notification.senderName, notification.postsId)
-            }
-          >
-            {/* 유저 프로필이미지 */}
-            <div className={styles.userImage}>
-              <Image
-                src={notification.senderProfileImg ?? '/svgs/profile.svg'}
-                fill
-                alt="유저이미지"
-              />
-            </div>
-
-            <div className={styles.infoContainer}>
-              {/* 유저이름 및 알람 유형 */}
-              <div className={styles.infoContainer__info}>
-                <span className={styles.infoContainer__name}>
-                  {notification.senderName}
-                </span>
-              </div>
-              {/* 댓글내용 및 게시글 내용 */}
-              <p className={styles.infoContainer__content}>
-                {/* 시안 1 */}
-                {/* {notification.content} */}
-                {/* 시안 2 */}
-                {notification.title}
-              </p>
-              {/* 게시글 제목 */}
-              <p className={styles.infoContainer__title}>
-                {/* 시안 1 */}
-                {/* {notification.title}  */}
-                {/* 시안 2*/}
-                {notification.type === 1
-                  ? '게시글을 작성하였습니다.'
-                  : '게시물에 댓글을 남겼습니다.'}
-              </p>
-            </div>
-
-            {/* 알람시간 */}
-            <div className={styles.time}>
-              <span>{notification.createAt}</span>
-              {isSelect && (
-                <CheckCircledIcon
-                  className={styles.time__icon}
-                  onClick={() => selecterId(notification.id)}
+      <div className={styles.notificationList}>
+        {notificationList.map((notification) => {
+          return (
+            <div key={notification.id} className={styles.notification}>
+              {/* 유저 프로필이미지 */}
+              <div className={styles.userImage}>
+                <Image
+                  src={notification.senderProfileImg ?? '/svgs/profile.svg'}
+                  fill
+                  alt="유저이미지"
                 />
-              )}
-              {/* 시안 1 */}
-              {/* <span>{notification.type === 1 ? '게시글' : '댓글'}</span> */}
+              </div>
+
+              <div
+                className={styles.infoContainer}
+                onClick={() =>
+                  blogPageRouter(notification.senderName, notification.postsId)
+                }
+              >
+                {/* 유저이름 및 알람 유형 */}
+                <div className={styles.infoContainer__info}>
+                  <span className={styles.infoContainer__name}>
+                    {notification.senderName}
+                  </span>
+                </div>
+                {/* 댓글내용 및 게시글 내용 */}
+                <p className={styles.infoContainer__content}>
+                  {/* 시안 1 */}
+                  {/* {notification.content} */}
+                  {/* 시안 2 */}
+                  {notification.title}
+                </p>
+                {/* 게시글 제목 */}
+                <p className={styles.infoContainer__title}>
+                  {/* 시안 1 */}
+                  {/* {notification.title}  */}
+                  {/* 시안 2*/}
+                  {notification.type === 1
+                    ? '게시글을 작성하였습니다.'
+                    : '게시물에 댓글을 남겼습니다.'}
+                </p>
+              </div>
+
+              {/* 알람시간 */}
+              <div className={styles.time}>
+                <span>{notification.createAt}</span>
+                {isCheck && (
+                  <CheckCircledIcon
+                    className={`${styles.time__icon} ${isSelectd(notification.id) ? styles.time__icon__select : ''}`}
+                    onClick={() => selecterId(notification.id)}
+                  />
+                )}
+                {/* 시안 1 */}
+                {/* <span>{notification.type === 1 ? '게시글' : '댓글'}</span> */}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
