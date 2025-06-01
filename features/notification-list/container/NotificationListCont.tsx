7a33f5010f4c38c1c9f64089f6bd08817d4e5937
junik <wkdwnsdlr37@gmail.com>
2025-06-01 19:10:02 +0900

    Refactor: notificationListCont 구조변경

--- features/notification-list/container/NotificationListCont.tsx
+++ features/notification-list/container/NotificationListCont.tsx
@@ -1,50 +1,44 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { NotificationItemType } from '../types';
 import NotificationListPres from '../presentational/NotificationListPres';
+import { stripMarkdown } from '@/shared/utils/stripmarkdown';
 
 export type Notification = {
-  idx: number,
-  userImage: string | null,
-  userName: string,
-  notificationType: number,
-  content: string,
-  title: string,
-  date: string,
-  postsId: number,
-  isRead: boolean,
-}
+  id: number;
+  type: number;
+  postsId: number;
+  senderId: string;
+  checkStatus: number;
+  createAt: string;
+  title: string;
+  content: string;
+  senderName: string;
+  senderProfileImg: string | null;
+  comment_content: string | null;
+};
 
-export default function NotificationListCont(props) {
+export default function NotificationListCont() {
+  // 상위 props 대기
   const [isSelect, setIsSelect] = useState<boolean>(false);
+  // 상위 props 대기
   const [selectList, setSelectList] = useState<number[]>([]);
-  const clickSelecterId = ( idx: number) => {
-    setSelectList([...selectList, idx])
-  }
-  const [notificationList, setNotificationList] = useState<Notification[]>([{
-    idx: 1,
-    userImage: null,
-    userName: 'test1',
-    notificationType: 1,
-    content: "게시글 내용입니다.",
-    title: "게시글 제목",
-    date: '2025-05-01',
-    postsId: 1,
-    isRead: true,
-  },
-  {
-    idx: 2,
-    userImage: null,
-    userName: 'test2',
-    notificationType: 0,
-    content: "댓글 내용입니다.",
-    title: "게시글 제목",
-    date: '2025-05-01',
-    postsId: 2,
-    isRead: false,
-  }
-]);
+  const clickSelecterId = (idx: number) => {
+    setSelectList([...selectList, idx]);
+  };
 
-  return <NotificationListPres notificationList={notificationList} isSelect={isSelect} selecterId={clickSelecterId}/>;
+  const [notificationList, setNotificationList] = useState<Notification[]>([]);
+  useEffect(() => {
+    fetch('/api/member/notifications')
+      .then((res) => res.json())
+      .then((data) => setNotificationList(data.data));
+  }, []);
+
+  return (
+    <NotificationListPres
+      notificationList={notificationList}
+      isSelect={isSelect}
+      selecterId={clickSelecterId}
+    />
+  );
 }
