27ea39a8d22bbc1a030b88bd1f948178f0ac107a
junik <wkdwnsdlr37@gmail.com>
2025-06-02 06:19:18 +0900

    Feat: NotificationModal 생성

--- widgets/notification/container/NotificationModalCont.tsx
+++ widgets/notification/container/NotificationModalCont.tsx
@@ -1,36 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { NotificationListCont } from '@/features/notification-list';
 import NotificationModalPres from '../presentational/NotificationModalPres';
-
-/*
-  notification-list: GET요청 
-  notification-delete: DELETE 요청
-  nofification-check: PUT 요청
-
-
-  [widgets]
-  1. 체크된 알람을 알기위한 useState 필요
-  2. 체크 상태로 전환하기위한 useState 필요
-  3. 삭제 클릭시 DELETE 버튼 출력
-  4. 전체선택시,
-
-
-  [GET]
-  1. 자체적으로 GET요청을 하여 알람리스트를 그려야 한다.
-  2. 상위에서 체크전환, 체크알람리스트 useState를 props로 받아야함
-  
-  [DELETE]
-  1. widgets에서 체크된 알람리스트를 받아야함
-
-  [PUT]
-  1. 전체읽음은 상위에서 전달받을 Props는 존재하지않음
-  2. 개별적인 PUT은 알람ID를 Props로 받아야함
-
-*/
+import { NotificationReadCont } from '@/features/notification-read';
+import { NotificationDeleteCont } from '@/features/notification-delete'
 
 export default function NotificationModalCont() {
+
+  const { submitRead, isSuccess } = NotificationReadCont();
+  const { submitDelete, deleteStatus } = NotificationDeleteCont();
+  const { notificationList } = NotificationListCont(deleteStatus);
   
+  const [selectList, setSelectList] = useState<number[]>([]);
+  const clickSelecterId = (idx: number) => {
+    setSelectList((prev) =>
+      prev.includes(idx) ? prev.filter((id) => id !== idx) : [...prev, idx],
+    );
+  };
+
+  const allCheck = () => {
+    const allIds = notificationList.map(item => item.id);
+    setSelectList(allIds);
+  };
+
+  const allCancel = () => setSelectList([]);
 
   return (
-    <NotificationModalPres/>
+    <NotificationModalPres
+      allCheck={allCheck}
+      allCancel={allCancel}
+      notificationList={notificationList}
+      selectList={selectList}
+      clickSelecterId={clickSelecterId}
+      submitRead={submitRead}
+      isSuccess={isSuccess}
+      submitDelete={submitDelete}
+    />
   );
 }
