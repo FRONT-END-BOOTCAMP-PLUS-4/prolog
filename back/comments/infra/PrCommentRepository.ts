4df8997edb4e9d94a9914828462c3c6f958936a0
imi21123 <imi21123@naver.com>
2025-06-02 06:43:05 +0900

    Feat: 댓글 조회 백엔드 구현

--- back/comments/infra/PrCommentRepository.ts
+++ back/comments/infra/PrCommentRepository.ts
@@ -2,6 +2,7 @@ import prisma from '@/shared/lib/prisma';
 import { CreateCommentDto } from '../application/dto/CreateCommentDto';
 import { CommentRepository } from '../domain/CommentRepository';
 import { Comment } from '@/app/generated/prisma';
+import { GetCommentDto } from '../application/dto/GetCommentDto';
 
 export class PrCommentRepository implements CommentRepository {
   async createComment(newComment: CreateCommentDto): Promise<Comment> {
@@ -14,5 +15,33 @@ export class PrCommentRepository implements CommentRepository {
     });
 
     return createdComment;
+  }
+
+  async findAllByPostId(postId: number): Promise<GetCommentDto[]> {
+    const comments = await prisma.comment.findMany({
+      where: { postsId: postId },
+      orderBy: { createdAt: 'asc' },
+      include: {
+        user: {
+          select: {
+            profileImg: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    return comments.map((comment) => ({
+      id: comment.id,
+      postsId: comment.postsId,
+      userId: comment.userId,
+      content: comment.content,
+      createdAt: comment.createdAt,
+      updatedAt: comment.updatedAt ?? null,
+      profileImage: comment.user.profileImg,
+      nickname: comment.user.name,
+      userEmail: comment.user.email,
+    }));
   }
 }
