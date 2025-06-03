f0431705be7572ca9a922bcf3db8148a2c0cd13e
Jangs <108041161+JangIkIk@users.noreply.github.com>
2025-06-03 14:43:36 +0900

    Refactor: 빌드환경에서 조건 처리 (#111)

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -4,6 +4,8 @@ import Github from 'next-auth/providers/github';
 import prisma from '@/shared/lib/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
+
   session: {
     strategy: 'jwt',
     maxAge: 60 * 60,
