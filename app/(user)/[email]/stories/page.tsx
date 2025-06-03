c2198f5746bd3721d733ecbd404c3df066bfd719
mjhn010 <mjhn010@naver.com>
2025-06-03 17:36:48 +0900

    fix: 빌드 오류 수정

--- app/(user)/[email]/stories/page.tsx
+++ app/(user)/[email]/stories/page.tsx
@@ -5,16 +5,17 @@ import ProfileCardCont from '@/views/story/profile-card/container/ProfileCardCon
 //style
 import styles from './styles.module.scss';
 import CategoryListCont from '@/features/category-list/container/CategoryListCont';
-import MyBlogCardListCont from '@/views/story/myblog-card-list/container/MyBlogCardListCont';
-import { usePathname } from 'next/navigation';
+// import MyBlogCardListCont from '@/views/story/myblog-card-list/container/MyBlogCardListCont';
 import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 export default function Page() {
-  const pathname = usePathname();
-  const usernameEncoded = pathname.split('/')[1];
-  const username = decodeURIComponent(usernameEncoded);
   const [userId, setUserId] = useState<string | null>(null);
-
+  const { data: session, status } = useSession();
+  const id = session?.user.id;
+  const pathname = usePathname();
+  const username = pathname.split('/')[1];
   useEffect(() => {
     const fetchUserId = async () => {
       const res = await fetch(`/api/member?username=${username}`);
@@ -32,12 +33,15 @@ export default function Page() {
     <>
       <div className={styles.container}>
         <div className={styles.cardFollowFlex}>
-          <ProfileCardCont userId={userId as string} username={username} />
+          <ProfileCardCont
+            userId={userId as string}
+            username={username as string}
+          />
           <div className={styles.categoryCardList}>
             <CategoryListCont />
             {/* 임시 적으로 주석처리 */}
             <div className={styles.cardList}>
-              <MyBlogCardListCont />
+              {/* <MyBlogCardListCont userId={userId as string} id={id as string} /> */}
             </div>
           </div>
         </div>
