452c22c766406d8522a6ae18f02f81aedf0d64a6
jun Y <mjhn010@naver.com>
2025-06-04 15:54:42 +0900

    Refactor: validator edit (#133)
    * Comment: 주석제거
    
    * Feat: myBlog 정렬기준 bookmark불러오기 구현
    
    * Comment: 주석제거
    
    * Chore: getToken 을 auth 로 수정
    
    * Comment: 주석 삭제
    
    * Fix: 빌드 타입 오류  수정
    
    * Refactor: validator edit

--- shared/utils/validators/index.ts
+++ shared/utils/validators/index.ts
@@ -10,15 +10,10 @@ export const validateNumericParam = (
 // 정렬 파라미터 유효성 검사
 export const validateSortParam = (
   value: string | null,
-): 'latest' | 'popular' | 'bookMark' => {
-  if (value === 'popular') {
-    return 'popular';
-  }
-  if (value === 'bookMark') {
-    return 'bookMark';
-  }
-  return 'latest';
+): 'latest' | 'popular' => {
+  return value === 'popular' ? 'popular' : 'latest';
 };
+
 //my블로그
 export const myBlogValidateSortParam = (
   value: string | null,
