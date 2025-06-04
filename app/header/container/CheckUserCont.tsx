ceccd84f6df1b7288a1ab1269af3fe6bb53b72c8
서유덕 <awesome36@daum.net>
2025-06-04 18:26:33 +0900

    Fix: 헤더 프로필 이미지 session 대신 DB에서 가져오도록 변경하여 데이터 동기화 문제 해결

--- app/header/container/CheckUserCont.tsx
+++ app/header/container/CheckUserCont.tsx
@@ -1,16 +1,32 @@
 'use client';
 // package
-import { type JSX } from 'react';
+import { useEffect, useState, type JSX } from 'react';
 // slice
 import HeaderPres from '../presentational/HeaderPres';
 import { useSession } from 'next-auth/react';
-import { toast } from 'react-toastify';
-import { error } from 'console';
 
 export default function CheckUserCont(): JSX.Element {
   // 유저 체크 로직
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
+
+  const [profileImg, setProfileImg] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const response = await fetch(`/api/${session?.user.name}/stories`);
+
+      if (!response.ok) {
+        throw new Error('유저 정보 불러오기 실패');
+      }
+
+      const result = await response.json();
+
+      setProfileImg(result.profileImg);
+    };
+
+    getUserInfo();
+  }, [session]);
 
   const username = session?.user?.name ?? '';
-  return <HeaderPres username={username} />;
+  return <HeaderPres username={username} profileImg={profileImg} />;
 }
